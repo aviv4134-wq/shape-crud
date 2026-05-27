da0ee6f85891f5f3e4ae297af886565e37f8c67b
avraham <aviv4134@gmail.com>
2026-05-27 10:09:34 +0300

    add methoad get area of rectangle

--- rectangle.py
+++ rectangle.py
@@ -3,6 +3,16 @@ import shape
 
 class Rectangle(shape.Shape):
     
-    def __init__(self, shape_id, shape_type):
+    def __init__(self, shape_id, shape_type,side):
         super().__init__(shape_id, shape_type)
-    
+        self.side = side    
+    def get_area(self):
+        """calculate rectangle area
+         taken : none
+         return : rectangle area  """
+        rectangle_area = self.side ** 2
+        return rectangle_area
+    def get_perimeter(self):
+        pass
+    def to_dict(self):
+        pass
