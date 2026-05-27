ec0aff507de3e613e0780eb17ffa9bcd2f72dab0
avraham <aviv4134@gmail.com>
2026-05-27 10:41:57 +0300

    init class squre with methoads

--- square.py
+++ square.py
@@ -1,6 +1,26 @@
 import shape
 
 class Square(shape.Shape):
-    def __init__(self, shape_id, shape_type):
+    def __init__(self, shape_id, shape_type,side):
         super().__init__(shape_id,shape_type)
+        self.side = side
+    def get_area(self):
+        """calculate square area
+         taken : none
+         return : square area  """
+        square_area = self.side ** 2
+        return square_area
+    def get_perimeter(self):
+        """calculate square perimeter
+        taken : none
+        return : square perimeter"""
+        square_perimeter = self.side * 4
+        return square_perimeter 
+    def to_dict(self):
+        """add side parameter to dict of a shape
+         taken : none
+         return : dict """
+        data = super().to_dict()
+        data['side'] = self.side
+        return data
     
