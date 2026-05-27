5f4ba0200da5dbef4118ce5ac5d93721ac7b1e1e
avraham <aviv4134@gmail.com>
2026-05-27 17:03:42 +0300

    add method load from json

--- shape_manager.py
+++ shape_manager.py
@@ -1,5 +1,5 @@
-import circle,rectangle,square
-
+import circle,rectangle,square,json
+from logger_file import logger
 
 class ShapeManager:
     def __init__(self):
@@ -18,17 +18,40 @@ class ShapeManager:
             print(f'{shape}\n')
 
     def update_shape(self, shape_id, new_data):
-        pass
+        data = ShapeManager().load_from_json()
+        for shape in data:
+            if shape['id'] == shape_id:
+               if not shape.get('radius'):
+                   shape['side'] = new_data
+                   ShapeManager().save_to_json()
+                   return None               
+               else:
+                   shape['radius'] = new_data
+                   ShapeManager().save_to_json()
+                   return None
+            
+
+
+            
     def delete_shape(self, shape_id):
         pass
     def save_to_json(self):
         pass
     def load_from_json(self):
-        pass
+        with open('shapes.json','r') as f:
+           data = json.load(f)
+           return(data)
 
 
 if __name__ == '__main__':
     manager = ShapeManager()
     manager.create_shape(circle.c)
     manager.create_shape(circle.t)
-    manager.get_all_shapes()
+    #manager.get_all_shapes()
+    print(manager.load_from_json())
+    manager.update_shape(1,1)
+    print(manager.load_from_json())
+
+    #with open('shapes.json','r') as f:
+     #   q=json.load(f)
+      #  print(q)
