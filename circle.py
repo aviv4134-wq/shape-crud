3d8981bd2313b50cf1477d61a162ab395971d353
avraham <aviv4134@gmail.com>
2026-05-27 11:58:23 +0300

    add docstring to methods

--- circle.py
+++ circle.py
@@ -8,12 +8,27 @@ class Circle(shape.Shape):
         self.radius = radius
     
     def get_area(self):
+        """calculate circle area
+         taken : none
+         return : circle area  """
         return Circle.PI * self.radius **2
     
     def get_perimeter(self):
+        """calculate circle perimeter
+        taken : none
+        return : circle perimeter"""
         return Circle.PI * self.radius * 2
     
     def to_dict(self):
+        """add side parameter to dict of a shape
+         taken : none
+         return : dict """
         data = super().to_dict()
         data['radius'] = self.radius
-        return data  
+        return data
+    
+    #def __str__(self) -> str:
+        Circle.get_perimeter(self)
+        return self.shape_id,self.shape_type,self.radius 
+
+c = Circle(6,'circle',5)
